7733923cf990fe2f779f7c4edebe7990f9ba4b46
Prasanna <prasanna.venkatesan@accoliteindia.com>
2019-04-14 18:19:49 +0530

    Updates

---
+++ precache-manifest.33b7a92f81d3a32e8179b85e6ce495f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactPage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5c814c687d02ec2791b",
+    "url": "/reactPage/static/js/runtime~main.30d5b25c.js"
+  },
+  {
+    "revision": "ecb8a1a6cd7a2f9673f3",
+    "url": "/reactPage/static/js/main.2b27f207.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/reactPage/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ecb8a1a6cd7a2f9673f3",
+    "url": "/reactPage/static/css/main.d1c893aa.chunk.css"
+  },
+  {
+    "revision": "1bc3787f4f4406f9e7839592c4347cd3",
+    "url": "/reactPage/index.html"
+  }
+];
